23573c5a72d72793ad62ab618be9fe20236a7d59
Ramla Ijaz <ijazramla@gmail.com>
2024-08-05 12:54:48 -0400

    usize spec for alignment

--- kernel/prusti_external_spec/src/num.rs
+++ kernel/prusti_external_spec/src/num.rs
@@ -4,7 +4,7 @@ use prusti_contracts::*;
 impl usize {
     #[pure]
     #[ensures(match result {
-        Some(x) => x == self + rhs,
+        Some(x) => (x == self + rhs) && forall(|y: usize| self % y == 0 && rhs % y == 0 ==> x % y == 0),
         None => true
     })]
     pub fn checked_add(self, rhs: usize) -> Option<usize>;
